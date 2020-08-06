c1228a796512a396d648b792c7c8245d2358d315
LuanSouzaSilveira <luansouzasilveira@gmail.com>
2020-08-06 12:21:35 -0300

    PageHeader

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
 import styles from './styles';
 
 const Favorites = () => {
   return (
-    <View style={styles.container}/>
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos"/>
+    </View>
   );
 }
 
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 const TeacherList = () => {
   return (
-    <View style={styles.container}/>
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis"/>
+    </View>
   );
 }
 
